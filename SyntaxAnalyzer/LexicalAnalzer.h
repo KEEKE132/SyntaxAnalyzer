3a07b068f504c7c6f0f7f3aa60191d069998fe91
Feel5nD <dksvlfdhs1@naver.com>
2023-10-27 20:59:00 +0900

    초기 커밋.

---
+++ SyntaxAnalyzer/LexicalAnalzer.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<string>
+
+int start(std::string);
+int lookup(char);
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
---
+++ SyntaxAnalyzer/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include "LexicalAnalzer.h"
+
+using namespace std;
+
+extern ifstream file;
+extern int nextToken;
+
+int main(int argc, char* argv[]) {
+	//string filename = argv[1];
+	file.open("test.txt");
+	if (!file.is_open()) {
+		cout << "ERROR - file is not open" << endl;
+	}
+	else {
+		getChar();
+		do {
+			lex();
+		} while (nextToken != EOF);
+	}
+	return 0;
+}
